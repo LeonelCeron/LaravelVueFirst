1f8b0c5f4df806adf6f6f185ad2bd794c78c2210
LeonelCeron <ceronleonel99@gmail.com>
2024-03-20 16:59:34 -0600

    agregando Schema::defaultStringLength(191) a provider para migraciones

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,14 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+
     public function register()
     {
         //
@@ -23,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
     }
 }
